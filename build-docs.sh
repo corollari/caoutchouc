0c939b77cc8963212ba19eda16f5d68749f0df50
Albert <github@albert.sh>
2019-06-14 23:18:57 +0200

    Remove unneeded instruction

--- build-docs.sh
+++ build-docs.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 set -ev
 cd docs
-mkdir output
 pandoc -t html --css "pandoc.css" -s -M title="Caoutchouc" -V title:"" index.md -o index.html -V header-includes:"<link rel='shortcut icon' href='favicon.png' />" --fail-if-warnings
